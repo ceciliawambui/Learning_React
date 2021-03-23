1829b59492e725e0dc1097284cfe7388b7273b8c
ceciliawambui <ceciliawambui026@gmail.com>
2021-03-23 08:09:20 +0300

    Learnt on Javascript variables

---
+++ person/variables.js
@@ -0,0 +1,12 @@
+//constant variables, no new value can be assigned
+const myNam="Mercy";
+console.log(myNam)
+
+//no commonly used
+var myName="Cecilia";
+console.log(myName)
+
+//can reassign value
+let myPet="cat";
+myPet="dog";
+console.log(myPet)
