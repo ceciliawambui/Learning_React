dd163b4060f2060568ceec7eecc78122cd35c79c
ceciliawambui <ceciliawambui026@gmail.com>
2021-03-23 08:20:34 +0300

    Learnt about Javascript functions

---
+++ person/functions.js
@@ -0,0 +1,9 @@
+//has two arguments
+let printMyName = (name,age) =>{
+    console.log(name, age)
+}
+printMyName("Cess", "20");
+
+//to persorm calculation, shortest way is
+const multiply=number=>number*2;
+console.log(multiply(2));
